9de934a2f7e51c0dec0ebebe80423bc00732e67f
Siarhei87 <dolph-1@yandex.ru>
2021-04-25 13:49:27 +0300

    Initial Commit

---
+++ TestNewsApp/TestNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewsAppApp.swift
+//  TestNewsApp
+//
+//  Created by Siarhei Dubko on 25.04.21.
+//
+
+import SwiftUI
+
+@main
+struct TestNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
